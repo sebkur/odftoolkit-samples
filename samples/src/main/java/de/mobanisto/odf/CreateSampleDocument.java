b2a7055ad1e371266d9baa5765eecaa8d0a4154c
Sebastian Kürten <sebastian@topobyte.de>
2020-01-30 22:22:21 +0100

    Refactor package name of samples module

--- samples/src/main/java/de/mobanisto/odf/CreateSampleDocument.java
+++ samples/src/main/java/de/mobanisto/odf/CreateSampleDocument.java
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package de.mobanisto;
+package de.mobanisto.odf;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
